7f7f3306f0615334902957a77da76d26e150fbad
ElDanteDelAnte <sreddell@att.net>
2016-11-11 23:23:35 -0800

    Game Manager
    Adds Ground layer and Ground prefab. Adds level loader.

--- SecondAttempt/SecondVersion/Assets/Scripts/GameManager.cs
+++ SecondAttempt/SecondVersion/Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class GameManager : MonoBehaviour
@@ -11,12 +12,30 @@ public class GameManager : MonoBehaviour
         inst = this;
     }
 
-    public bool playerPaused;       //whether the game is paused
+    //selected character prefabs
+    public GameObject heavyPlayer;
+    public GameObject lightPlayer;
+
+    //game levels
+    public int level;
+    public Level[] levels;
+
+    //game screens
+    public Scene startScreen;       //start menu
+    public Scene goScreen;          //game over screen
+    public Scene pauseScreen;       //pause screen
+
+    //public bool playerPaused;       //whether the game is paused
 
     // Use this for initialization
     void Start ()
     {
-        playerPaused = false;
+        //playerPaused = false;
+    }
+
+    public void loadLv(int lv)
+    {
+        levels[lv].loadLevel();
     }
     
     // Update is called once per frame
---
+++ SecondAttempt/SecondVersion/Assets/Scripts/Level.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+[CreateAssetMenu(menuName = "Level")]
+public class Level : ScriptableObject
+{
+    public Vector2 heavyStart;
+    public Vector2 lightStart;
+
+    public int levLindex;
+
+    /// <summary>
+    /// Loads the level's scene and spawns the player characters.
+    /// </summary>
+    public void loadLevel()
+    {
+        SceneManager.LoadScene(levLindex, LoadSceneMode.Single);
+
+        //change these to corresponding characters
+        GameObject hev = Instantiate(GameManager.inst.heavyPlayer, heavyStart, Quaternion.identity) as GameObject;
+        GameObject lit = Instantiate(GameManager.inst.lightPlayer, lightStart, Quaternion.identity) as GameObject;
+
+        //add behaviors?
+    }
+}
