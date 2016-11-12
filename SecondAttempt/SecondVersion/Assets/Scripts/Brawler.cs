65f7596d268894a6c9caf060958c0b41eee19251
ElDanteDelAnte <sreddell@att.net>
2016-11-11 19:20:36 -0800

    Starting Over\n\nAlso, it looks like .gitignore may actually work now

---
+++ SecondAttempt/SecondVersion/Assets/Scripts/Brawler.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Brawler : MonoBehaviour
+{
+    public int health;          //brawler's current health
+    private int maxHeath;       //brawler's maximum health
+    private int atkDam;         //brawler's attack damage
+    
+    
+    
+    public bool facingRight;    //brawler's direction
+
+    // Use this for initialization
+    void Start ()
+    {
+    
+    }
+	
+    // Update is called once per frame
+    void FixedUpdate ()
+    {
+        //movement
+        //attack
+    }
+
+    //either melee or ranged
+    public void attack()
+    {
+
+    }
+}
