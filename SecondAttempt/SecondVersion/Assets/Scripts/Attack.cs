9e99c8156262354d700c743c5dc37111da2bbc61
ElDanteDelAnte <sreddell@att.net>
2016-11-11 20:04:09 -0800

    Basic Framework for Brawler and Attack

---
+++ SecondAttempt/SecondVersion/Assets/Scripts/Attack.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attack : MonoBehaviour
+{
+    public bool ranged;    //determines if the attack will move from its starting position
+    public int damage;     //the strength of the attack
+    public int duration;   //how long the attack is supposed to exist
+    private int elapsed;    //how long the attack has existed
+
+	// Use this for initialization
+	void Start ()
+    {
+        elapsed = 0;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+	    //check collision
+        //move
+	}
+}
