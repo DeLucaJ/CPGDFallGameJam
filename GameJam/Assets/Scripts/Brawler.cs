9d4c5dfed2dceb53672d45bdba1ac201d0f67b10
ElDanteDelAnte <sreddell@att.net>
2016-11-11 16:18:04 -0800

    Falling Robot
    Adds Game Object with Rigidbody.

---
+++ GameJam/Assets/Scripts/Brawler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Brawler : MonoBehaviour
+{
+    public int health;      //brawler's current health
+    private int maxHeath;   //brawler's maximum health
+    
+
+    // Use this for initialization
+    void Start ()
+    {
+    
+    }
+	
+    // Update is called once per frame
+    void Update ()
+    {
+	
+    }
+}
